4578d1ff069826e82677f2aa850aa7eb4e6110a5
AnaOstio <anafdezostio01@gmail.com>
2022-12-03 17:24:15 +0100

    ejercicio 13 js

--- Entrega-02/Ejercicio-13/Tarea-1/MapaKML.js
+++ Entrega-02/Ejercicio-13/Tarea-1/MapaKML.js
@@ -1,24 +1,15 @@
 "use strict"
 class MapaKML{
-    constructor(){}
+    constructor(){
+    }
 
     initMap(){
 
         let centro = { lat: 43.52622, lng: -5.65567 };
-            let mapa = new google.maps.Map(document.querySelector("main"), 
+             this.mapa = new google.maps.Map(document.querySelector("main"), 
                     {zoom: 8, center: centro, mapTypeId: google.maps.MapTypeId.SATELLITE});
 
         let win = new google.maps.InfoWindow();
-
-        this.lugares.forEach(function(lugar) {
-            let posicion = lugar.toString().split(","); 
-            posicion = { lat: Number(posicion[1]), lng: Number(posicion[0]) };
-
-            let marcador = new google.maps.Marker({
-                map: mapa,
-                position: posicion
-            });
-        }.bind(this));
 
     }
 
@@ -54,6 +45,60 @@ class MapaKML{
 
         } else {
             $('p').text("Solo se aceptan archivos de tipo geojson")
+        }
+    }
+
+
+    checkFile2(files){
+        let archivo = files[0]
+        let lector = new FileReader();
+
+        let tipo = archivo.name
+        var th = this;
+        var ar = new Array();
+        
+        if(tipo.match(/.kml/)){
+
+            lector.onload = function(evento) {
+                var a = lector.result
+                let b = $('coordinates', a).text().split(',')
+                let c = new Array();
+                for(var r=0; r< b.length; r++){
+
+
+                    if(r % 2 == 0 && r > 0){
+                        b[r] = b[r].toString().substring(3)
+                    }
+                }
+
+                b.pop()
+
+                for(var i = 0; i < b.length - 2; i++){
+                    var cood =  {lat: Number(b[i + 1]), lng: Number(b[i])}
+                    i++;
+                    c.push(cood)
+                }
+
+                th.puntos(c)
+            }
+
+            lector.readAsText(archivo)
+
+        } else {
+            $('p').text("Solo se aceptan archivos de tipo geojson")
+        }
+    }
+
+    puntos(datos){
+        let centro = { lat: 43.52622, lng: -5.65567 };
+             this.mapa = new google.maps.Map(document.querySelector("main"), 
+                    {zoom: 8, center: centro, mapTypeId: google.maps.MapTypeId.SATELLITE});
+
+        let win = new google.maps.InfoWindow();
+        for(let i = 0; i < datos.length; i++){
+            var marc2 = new google.maps.Marker({
+                position: datos[i], map:this.mapa
+            })
         }
     }
 
--- Entrega-02/Ejercicio-14/Ejercicio14.js
+++ Entrega-02/Ejercicio-14/Ejercicio14.js
@@ -1,7 +1,96 @@
 class Audio{
 
+    constructor(){
+        this.contexto;
+        this.source;
+        this.filter;
+        this.cont = 0
+    }
 
-    
+    checkFile(files){
+        this.procesaAudio(files[0])
+
+    }
+
+    procesaAudio(file){
+        // Tipo del archiuo
+        var tipo = /.mp3*/
+        if(!file.name.match(tipo)){
+            $('section').after("<p>El fichero " + file.name + " no es un ficehro .mp3</p>")
+            return;
+        } else {
+            
+            var direccionArchivo = URL.createObjectURL(file);
+            
+            $('section').after(
+                "<article>" + 
+                    "<h2>El Nombre del audio es: " + file.name  + "</h2>" + 
+                    "<p>Opciones a mostrar</p>" +
+                    "<p>" +  
+                        "<input type = 'button' value = 'Lowpass' onclick = 'audio.lowpass()' />" + 
+                        "<input type = 'button' value = 'HighPass' onclick = 'audio.highpass()' />" +
+                        "<input type = 'button' value = 'Bandpass' onclick = 'audio.bandpass()' />" +
+                        "<input type = 'button' value = 'Notch' onclick = 'audio.notch()' />"
+                    + "</p>" +
+                    "<label for='hz'>Control Frecuencia</label>" +
+                    "<input id='hz' name = 'hz' type = 'range' value = '50' min = '1' max = '6000' onchange = 'audio.frecuencia()' />" +
+                    '<audio src = ' + direccionArchivo + ' controls </audio>' + 
+                "</article>"
+            )
+
+            this.contexto = new AudioContext()
+            this.source = this.context.createMediaElementSource(document.getElementsByTagName('audio')[0]);
+            this.filter = this.context.createBiquadFilter();
+            this.filter.type = "highpass"
+            this.source.connect(this.filter);
+            this.filter.connect(this.context.destination);
+        }
+    }
+
+    drop(event){
+        event.preventDefault();
+        var files = event.dataTransfer.files;
+        this.procesaAudio(files[0])
+    }
+
+    drag(event){
+        event.preventDefault();
+    }
+
+    lowpass() {
+        this.filter = this.contexto.createBiquadFilter();
+        this.filter.type = "lowpass"; 
+        this.source.connect(this.filter);
+        this.filter.connect(this.contexto.destination);
+    }
+
+    highpass() {
+        this.filter = this.contexto.createBiquadFilter();
+        this.filter.type = "highpass"; 
+        this.source.connect(this.filter);
+        this.filter.connect(this.contexto.destination);
+    }
+
+    bandpass() {
+        this.filter = this.contexto.createBiquadFilter();
+        this.filter.type = "bandpass"; 
+        this.source.connect(this.filter);
+        this.filter.connect(this.contexto.destination);
+    }
+
+    notch() {
+        this.filter = this.contexto.createBiquadFilter();
+        this.filter.type = "notch"; 
+        this.source.connect(this.filter);
+        this.filter.connect(this.contexto.destination);
+    }
+
+    frecuencia(){
+        var value = document.getElementsByName("hz").value;
+        this.filter.frequency.setValueAtTime(value,this.contexto.currentTime);
+        this.source.connect(this.filter);
+        this.filter.connect(this.contexto.destination);
+    }
 
 }
 
