9cea2119bb9c861f41e39aaa25c758f41d6a7248
AnaOstio <anafdezostio01@gmail.com>
2022-12-04 11:08:54 +0100

    ej14

--- Entrega-02/Ejercicio-14/Ejercicio14.js
+++ Entrega-02/Ejercicio-14/Ejercicio14.js
@@ -1,48 +1,26 @@
 class Audio{
 
     constructor(){
-        this.contexto;
+        this.context;
         this.source;
         this.filter;
         this.cont = 0
     }
 
     checkFile(files){
-        this.procesaAudio(files[0])
-
+        this.procesador(files[0])
     }
 
-    procesaAudio(file){
-        // Tipo del archiuo
-        var tipo = /.mp3*/
-        if(!file.name.match(tipo)){
-            $('section').after("<p>El fichero " + file.name + " no es un ficehro .mp3</p>")
-            return;
+    procesador(file){
+        if(!file.name.match(/.mp3*/)){
+            $('h3').before('<h3>El formato del archivo no es correcto</h3>')
         } else {
-            
-            var direccionArchivo = URL.createObjectURL(file);
-            
-            $('section').after(
-                "<article>" + 
-                    "<h2>El Nombre del audio es: " + file.name  + "</h2>" + 
-                    "<p>Opciones a mostrar</p>" +
-                    "<p>" +  
-                        "<input type = 'button' value = 'Lowpass' onclick = 'audio.lowpass()' />" + 
-                        "<input type = 'button' value = 'HighPass' onclick = 'audio.highpass()' />" +
-                        "<input type = 'button' value = 'Bandpass' onclick = 'audio.bandpass()' />" +
-                        "<input type = 'button' value = 'Notch' onclick = 'audio.notch()' />"
-                    + "</p>" +
-                    "<label for='hz'>Control Frecuencia</label>" +
-                    "<input id='hz' name = 'hz' type = 'range' value = '50' min = '1' max = '6000' onchange = 'audio.frecuencia()' />" +
-                    '<audio src = ' + direccionArchivo + ' controls </audio>' + 
-                "</article>"
-            )
-
-            this.contexto = new AudioContext()
+            $('audio').attr("src", URL.createObjectURL(file))
+            this.context = new AudioContext();
             this.source = this.context.createMediaElementSource(document.getElementsByTagName('audio')[0]);
-            this.filter = this.context.createBiquadFilter();
-            this.filter.type = "highpass"
-            this.source.connect(this.filter);
+            this.filter = this.context.createBiquadFilter()
+            this.filter.type = "highpass";
+            this.source.connect(this.filter)
             this.filter.connect(this.context.destination);
         }
     }
@@ -50,7 +28,7 @@ class Audio{
     drop(event){
         event.preventDefault();
         var files = event.dataTransfer.files;
-        this.procesaAudio(files[0])
+        this.procesador(files[0])
     }
 
     drag(event){
@@ -58,38 +36,38 @@ class Audio{
     }
 
     lowpass() {
-        this.filter = this.contexto.createBiquadFilter();
+        this.filter = this.context.createBiquadFilter();
         this.filter.type = "lowpass"; 
         this.source.connect(this.filter);
-        this.filter.connect(this.contexto.destination);
+        this.filter.connect(this.context.destination);
     }
 
     highpass() {
-        this.filter = this.contexto.createBiquadFilter();
+        this.filter = this.context.createBiquadFilter();
         this.filter.type = "highpass"; 
         this.source.connect(this.filter);
-        this.filter.connect(this.contexto.destination);
+        this.filter.connect(this.context.destination);
     }
 
     bandpass() {
-        this.filter = this.contexto.createBiquadFilter();
+        this.filter = this.context.createBiquadFilter();
         this.filter.type = "bandpass"; 
         this.source.connect(this.filter);
-        this.filter.connect(this.contexto.destination);
+        this.filter.connect(this.context.destination);
     }
 
     notch() {
-        this.filter = this.contexto.createBiquadFilter();
+        this.filter = this.context.createBiquadFilter();
         this.filter.type = "notch"; 
         this.source.connect(this.filter);
-        this.filter.connect(this.contexto.destination);
+        this.filter.connect(this.context.destination);
     }
 
     frecuencia(){
         var value = document.getElementsByName("hz").value;
-        this.filter.frequency.setValueAtTime(value,this.contexto.currentTime);
+        this.filter.frequency.setValueAtTime(value,this.context.currentTime);
         this.source.connect(this.filter);
-        this.filter.connect(this.contexto.destination);
+        this.filter.connect(this.context.destination);
     }
 
 }
